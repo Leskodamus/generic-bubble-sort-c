3ba6d1676c53c9c3eb97c81cf12d220a46d47597
Leskodamus <florian@leskovsek.dev>
2023-08-05 18:33:08 +0200

    Fix callback function name

--- example.c
+++ example.c
@@ -10,7 +10,7 @@ int main(void) {
     int arr[] = { 3, 2, 1, 5, 6, 4, 10, 9 };
     size_t arr_len = sizeof(arr) / sizeof(arr[0]);
     
-    sort(arr, arr_len, sizeof(arr[0]), compare_int);
+    sort(arr, arr_len, sizeof(arr[0]), cmp_int);
     
     for (size_t i = 0; i < arr_len; ++i)
         printf("%d ", arr[i]);
