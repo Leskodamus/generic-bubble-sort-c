de649e9d8bb2eabaf174c94c46a21144c79d04b2
Leskodamus <florian@leskovsek.dev>
2023-08-14 10:47:13 +0200

    feat: make header-only lib

--- generic_bubble_sort.h
+++ generic_bubble_sort.h
@@ -1,23 +1,36 @@
 #ifndef GENERIC_BUBBLE_SORT_LIB
 #define GENERIC_BUBBLE_SORT_LIB
 
-#include "stddef.h"
+#include <stddef.h>
 
-/**
- * 
- * GENERIC BUBBLE SORT LIBRARY
- *
- */
+static inline void swap(void* restrict v1, void* restrict v2, size_t p_size) {
+    unsigned char *p = v1, *q = v2, tmp;
+    for (size_t i = 0; i < p_size; ++i) {
+        tmp = p[i];
+        p[i] = q[i];
+        q[i] = tmp;
+    }
+}
 
-/* Swap the values of two given pointers */
-void swap(void* restrict v1, void* restrict v2, size_t p_size);
+extern inline void sort(void *arr, size_t n, size_t value_size, 
+          int (*cmp)(void*,void*)) 
+{
+    for (size_t i = 0; i < n; ++i) {
+        for (size_t j = 0; j < n - i - 1; ++j) {
+            unsigned char *l = ((unsigned char*)arr) + j * value_size;
+            unsigned char *r = ((unsigned char*)arr) + (j+1) * value_size;
+            if (cmp(l, r) > 0)
+                swap(l, r, value_size);
+        }
+    }
+}
 
-/* Bubble sort an array */
-void sort(void *arr, size_t n, size_t value_size, 
-          int (*cmp)(void*,void*));
+extern inline int cmp_int(void *v1, void *v2) {
+    int *p = v1, *q = v2; 
+    if (*p > *q) return 1;
+    else if (*p < *q) return -1;
+    else return 0;
+}
 
-/* Comparator for int */
-int cmp_int(void *v1, void *v2);
-
-#endif // GENERIC_BUBBLE_SORT_LIB
+#endif  // GENERIC_BUBBLE_SORT_LIB
 
